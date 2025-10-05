01d5618794771f6f0d7fbc33dd5f80e5cebf74ef
alessio <alessio.falasca0210@gmail.com>
2025-10-05 23:38:28 +0200

    first steps of part 1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+#include <cstdio>
+#include <GLFW/glfw3.h>
+#include <GL/glew.h>
 
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(){
+	glfwSetErrorCallback(error_callback);
+}
